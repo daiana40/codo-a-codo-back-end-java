2718d4c6f0ca9ab611c01036c68581af781fa5ec
daiana40 <daiana.a.herrera@gmail.com>
2022-07-14 21:47:07 -0300

    postgres

--- src/script-postgres.sql
+++ src/script-postgres.sql
@@ -1,9 +1,9 @@
-CREATE TABLE prodcuto (
+CREATE TABLE producto (
 	id SERIAL NOT NULL,
 	nombre VARCHAR(50) NOT NULL,
 	precio FLOAT NOT NULL, 
 	fecha_creacion DATE NOT NULL, 
-	imagen VARCHAR(100) NULL DEFAILT NULL,
+	imagen VARCHAR(100) NULL DEFAULT NULL,
 	codigo VARCHAR(7) NOT NULL, 
 	PRIMARY KEY(id),
 );
